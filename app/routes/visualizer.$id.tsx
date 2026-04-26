3dcc72120abbd81f485f8747ca2747613393bb7d
ryan <ryan@MacBook-Pro.local>
2026-04-26 19:36:59 +0100

    Generate 3D design

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,21 +1,116 @@
-import { useLocation } from "react-router";
+import { useRef, useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { generate3DView } from "../../lib/ai.action";
+import { Box, Download, RefreshCcw, Share, X } from "lucide-react";
+import { Button } from "../../components/ui/Button";
 
 const VisualizerId = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const { initialImage, name } = location.state || {};
-  return (
-    <section>
-      <h1>{name || "Untitled Project"}</h1>
+  const { initialImage, initialRendered, name } = location.state || {};
 
-      <div className="visualizer">
-        {initialImage && (
-          <div className="image-container">
-            <h2>Source Image</h2>
-            <img src={initialImage} alt="Source" />
-          </div>
-        )}
-      </div>
-    </section>
+  const hasInitialGenerated = useRef(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(
+    initialRendered || null,
+  );
+
+  const handleBack = () => navigate("/");
+
+  const runGeneration = async () => {
+    if (!initialImage) return;
+
+    try {
+      setIsProcessing(true);
+      const result = await generate3DView({ sourceImage: initialImage });
+
+      if (result.renderedImage) {
+        setCurrentImage(result.renderedImage);
+
+        //Upafe the project with the rendered Image
+      }
+    } catch (e) {
+      console.error("Generation failed", e);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!initialImage || hasInitialGenerated.current) return;
+
+    if (initialRendered) {
+      setCurrentImage(initialRendered);
+      hasInitialGenerated.current = true;
+      return;
+    }
+    hasInitialGenerated.current = true;
+    runGeneration();
+  }, [initialImage, initialRendered]);
+  return (
+    <div className="visualizer">
+      <nav className="topbar">
+        <div className="brand">
+          <Box className="logo" />
+          <span className="name">Roomify</span>
+        </div>
+        <Button variant="ghost" onClick={handleBack} className="exit">
+            <X className="icon"/> Exit Editor
+        </Button>
+      </nav>
+      <section className="content">
+        <div className="panel">
+             <div className="panel-header">
+            <div className="panel-meta">
+                <p>Project</p>
+                <h2>{'Untitled Project'}</h2>
+                <p className="note">Created by You</p>
+            </div>
+            <div className="panel-actions">
+                <Button
+                size="sm"
+                onClick = {() => {}}
+                className="export"
+                disabled={!currentImage}>
+                    <Download className="w-4 h-4 mr-2"/> Export
+                </Button>
+                <Button
+                size="sm"
+                onClick = {() => {}}
+                className="share">
+                    <Share className="w-4 h-4 mr-2"/>
+                    Share
+                </Button>
+            </div>
+        </div>
+
+        <div className={`render-area ${isProcessing ? 'is-processing':
+            ''}`}>
+                {currentImage ? (
+                    <img src={currentImage} alt="AI Render"
+                    className="render-img"/>
+                ): (
+                    <div className="render-placeholder">
+                        {initialImage && (
+                            <img src={initialImage} alt="Original"
+                            className="render-fallback" />
+                        )}
+
+                        {isProcessing && (
+                            <div className="render-overlay">
+                                <div className="rendering-card">
+                                    <RefreshCcw className="spinner"/>
+                                    <span className="title">Rendering...</span>
+                                    <span className="subtitle">Please wait while we generate your 3D view.</span>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
+        </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
